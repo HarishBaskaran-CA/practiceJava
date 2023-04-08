8735a8a91fa9cecff2d81325f57acea6c7b6fce7
HarishBaskaran-CA <harish.baskaran@go-yubi.com>
2023-04-08 21:40:15 +0530

    Commit message

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,80 @@
+package org.example;
+
+
+public class Main {
+    // class & objects
+    // variable
+        /*git add .
+            1. char
+            2. string
+            3. int
+            4. float
+            5. boolean
+            6. array
+         */
+    // methods
+    // Loops
+        /*
+            1. if else
+                a. else if
+                b. nested if
+            2. switch // less priority
+            3. for loop
+         */
+
+    public static void main(String[] args) {
+
+        // Rock paper scissors ?
+
+        String user1 = "rock";
+        String user2 = "rock";
+
+        if (user1.toLowerCase() == user2.toLowerCase()) {
+            System.out.println("retake");
+        } else if (user1.toLowerCase() == "rock") {
+            if (user2.toLowerCase() == "paper") {
+                System.out.println("user2 wins");
+            } else if (user2.toLowerCase() == "scissors") {
+                System.out.println("user1 wins");
+            }
+        } else if (user1.toLowerCase() == "paper") {
+            if (user2.toLowerCase() == "rock") {
+                System.out.println("user1 wins");
+            } else if (user2.toLowerCase() == "scissors") {
+                System.out.println("user2 wins");
+            }
+        } else if (user1.toLowerCase() == "scissors") {
+            if (user2.toLowerCase() == "rock") {
+                System.out.println("user2 wins");
+            } else if (user2.toLowerCase() == "paper") {
+                System.out.println("user1 wins");
+            }
+        }
+
+    }
+}
+
+/*
+        if(number <10 ) {
+            System.out.println("if condition");
+        }else if(10 < number && number <15){
+            System.out.println("else if condition");
+        }else if(10 < number && number <15){
+            System.out.println("else if condition");
+        }else if(10 < number && number <15){
+            System.out.println("else if condition");
+        }else if(10 < number && number <15){
+            System.out.println("else if condition");
+        } else {
+            System.out.println("else condition");
+        }
+ */
+
+// className objectName = new className();
+//        Runner object = new Runner();
+//        object.print();
+//        System.out.println(object.character);
+//        System.out.println(object.name);
+//        System.out.println(object.number);
+//        System.out.println(object.flag);
+//        System.out.println(object.decimal);
---
+++ src/main/java/org/example/Runner.java
@@ -0,0 +1,99 @@
+package org.example;
+
+public class Runner {
+
+    public char character = 'H';
+    public String name = "Harish";
+    public int number = 12;
+    public boolean flag = true;
+    public double decimal = 1.23;
+
+    public void print () {
+        System.out.println("Hi there i work from runner");
+    }
+
+    public void print2 (String text) {
+        System.out.println(text);
+    }
+
+    public void print2 (int value) {
+        System.out.println(value);
+    }
+}
+
+
+
+
+
+
+/*
+        if(number <10 ) {
+            System.out.println("if condition");
+        }else if(10 < number && number <15){
+            System.out.println("else if condition");
+        }else if(10 < number && number <15){
+            System.out.println("else if condition");
+        }else if(10 < number && number <15){
+            System.out.println("else if condition");
+        }else if(10 < number && number <15){
+            System.out.println("else if condition");
+        } else {
+            System.out.println("else condition");
+        }
+
+        if(month=="DEC"){
+           if(true){
+           }
+       }
+ */
+
+/*
+            switch(month){
+            case "JAN" :
+                System.out.println("31");
+                break;
+            case "FEB" :
+                System.out.println("28");
+                break;
+            default :
+                System.out.println("default");
+                break;
+        }
+ */
+
+/*
+   int i =0;
+        for( ;  i<12 ;  ) {
+            // some portion of code
+            i++;
+        }
+ */
+
+/*
+         int i =0;
+        while (i<12){
+
+            i++;
+        }
+
+        int i =0;
+       do{
+
+            i++;
+        } while ("password" != "stored password" && i<3);
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
